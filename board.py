622f33d009cf5f3bd9f9e20ce6fb16339c0f1b87
Tanmay-Gangwar <69584423+Tanmay-Gangwar@users.noreply.github.com>
2022-01-08 20:31:33 +0530

    Add files via upload

--- board.py
+++ board.py
@@ -23,15 +23,16 @@ class Board:
                     return (i, j)
         return None
     
-    def solve(self):
-        # create.display_screen()
+    def solve(self, screen, show):
+        if show: create.display_screen(screen, self, 0, 0, 0, 0, False)
+        # pygame.display.update()
         empty_pos = self.find_empty()
         if empty_pos is None: return True
         row, col = empty_pos
         for num in range(1, 10):
             if self.is_valid(num, row, col):
                 self.board[row][col][0] = num
-                if self.solve(): return True
+                if self.solve(screen, show): return True
                 self.board[row][col][0] = 0
         return False
 
